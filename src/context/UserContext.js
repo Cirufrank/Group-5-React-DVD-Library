79512601d7c869d64371c9ba34bfa9ab20f799ef
Cirũ Franklin <cirufrank@me.com>
2022-12-16 08:11:30 -0800

    Add Contact List template using React routing, navigation, hooks, and the Context API as template

---
+++ src/context/UserContext.js
@@ -0,0 +1,12 @@
+import {createContext} from "react";
+
+export const UserContext = createContext({userId: 1, userName: "guest"});
+
+export function UserProvider({children}) {
+    const userInfo = {
+        userId: 1,
+        userName: "Jean Johnson"
+    };
+
+    return <UserContext.Provider value={userInfo}>{children}</UserContext.Provider>
+}
